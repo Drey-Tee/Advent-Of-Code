9db278c4b43a8482ffb5dacb9edc691da4d8ceba
Drey-Tett <dreytetteh7@gmail.com>
2022-12-06 06:05:40 +0000

    day5-supply-stacks

---
+++ Day_05/supply_stacks_part1.py
@@ -0,0 +1,33 @@
+import re
+
+col = """TRGWQMFP
+RFH
+DSHGVRZP
+GWFBPHQ
+HJMSP
+LPRSHTZM
+LMNHTP
+RQDF
+HPLNCSD"""
+
+with open("input.txt", "r") as f:
+    data = f.read()
+
+output = 0
+
+initial, instructions = data.split('\n\n')
+crates = [list(l) for l in col.splitlines()]
+
+for l in instructions.split('\n'):
+    n, x, y = map(int, re.findall(r'\d+', l))
+    x -= 1
+    y -= 1
+    to_move = crates[x][:n]
+    crates[x] = crates[x][n:]
+    crates[y] = to_move[::-1] + crates[y]
+results = ""
+for i in range(len(crates)):
+    if len(crates[i]):
+        results += crates[i][0]
+
+print(results)
